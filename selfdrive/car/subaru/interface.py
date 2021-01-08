9ce5635cec46ad753393826723afff47093b6c74
letsdudiss <76602408+letsdudiss@users.noreply.github.com>
2021-01-08 17:18:18 +1100

    Updated Impreza steer tuning

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -49,9 +49,9 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 15
       ret.steerActuatorDelay = 0.4   # end-to-end angle controller
-      ret.lateralTuning.pid.kf = 0.00005
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      ret.lateralTuning.pid.kf = 0.00007843996051470903
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 14., 23.], [0., 14., 23.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.035, 0.04, 0.15], [0.04, 0.035, 0.045]]
 
     if candidate == CAR.FORESTER:
       ret.mass = 1568. + STD_CARGO_KG
