6239a17c4260cfc5bbc2f0328f8267db448d3373
letsdudiss <76602408+letsdudiss@users.noreply.github.com>
2021-01-08 16:58:55 +1100

    Workaround for 2020+ Subaru Impreza: Avoid steering torque exceeds car's limit causes EyeSight to go into Fault and become unavailable for the rest of the drive

--- selfdrive/car/subaru/carstate.py
+++ selfdrive/car/subaru/carstate.py
@@ -85,6 +85,7 @@ class CarState(CarStateBase):
       # sig_name, sig_address, default
       ("Steer_Torque_Sensor", "Steering_Torque", 0),
       ("Steering_Angle", "Steering_Torque", 0),
+      ("Steer_Warning", "Steering_Torque", 0),
       ("Cruise_On", "CruiseControl", 0),
       ("Cruise_Activated", "CruiseControl", 0),
       ("Brake_Pedal", "Brake_Pedal", 0),
